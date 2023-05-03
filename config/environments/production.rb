3fbd92b16b2c0ee7dafae3b68b1fef4a923c8618
Kadina1988 <kadina.aziev@mail.ru>
2023-05-03 23:13:26 +0300

    update profuction

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,12 +61,12 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "new_app_production"
-
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    post:                 587,
+    address:              'smtp.mail.ru',
+    port:                 993,
     user_name:            ENV['SMTP_USERNAME'],
     password:             ENV['SMTP_PASSWORD'],
     authentication:       'plain',
