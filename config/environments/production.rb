f9944ae672cf24fcac62ae3d8e5ea56a0c170898
Kadina1988 <kadina.aziev@mail.ru>
2023-05-03 09:58:21 +0300

    update delivery_method

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "new_app_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.delivety_method = :smtp
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     post:                 587,
