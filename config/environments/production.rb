279f8cb339d561e6df059b8a471ca6b158e15e79
Kadina1988 <kadina.aziev@mail.ru>
2023-05-04 22:04:03 +0300

    upd

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,16 +62,16 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "new_app_production"
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.action_mailer.perform_caching = false
+  # config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.yandex.ru',
     port:                 465,
-    domain:               ENV['DOMAIN'],
+    domain:               'yandex.ru',
     user_name:            ENV['SMTP_USERNAME'],
     password:             ENV['SMTP_PASSWORD'],
     authentication:       :plain,
-    ssl:                  true,
+    # ssl:                  true,
     tls:                  true,
     enable_starttls_auto: true }
 
