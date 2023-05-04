e7089098b92cff303ff5f60530a25cfc762c0c26
Kadina1988 <kadina.aziev@mail.ru>
2023-05-04 22:39:55 +0300

    ssl

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
     user_name:            ENV['SMTP_USERNAME'],
     password:             ENV['SMTP_PASSWORD'],
     authentication:       :plain,
-    # ssl:                  true,
+    ssl:                  true,
     tls:                  true,
     enable_starttls_auto: true }
 
