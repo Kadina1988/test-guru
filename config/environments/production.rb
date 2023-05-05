52d80e87b8410397c26aca29b051cb50d9b225af
Kadina1988 <kadina.aziev@mail.ru>
2023-05-05 21:24:11 +0300

    user name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,9 +61,9 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "new_app_production"
-  config.action_mailer.default_url_options = { host: 'mail.google.com/mail/u/0/#inbox' }
+  config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'test1-bocz.onrender.com' }
   config.action_mailer.perform_deliveries = true
-  # config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     tls:                  true,
@@ -72,10 +72,8 @@ Rails.application.configure do
     domain:               "yandex.com",
     authentication:       "plain",
     enable_starttls_auto: true,
-    user_name:            'aziev.kadina@yandex.ru',
-    password:             'komsomolsknaamu'
-
-  }
+    user_name:            'aziev.kadina',
+    password:             'komsomolsknaamu' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
