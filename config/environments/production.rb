f4f0b64a7d6116995ba6c3b73ca9516a5123ba7e
Kadina1988 <kadina.aziev@mail.ru>
2023-05-06 22:46:58 +0300

    smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,8 +71,8 @@ Rails.application.configure do
     domain:               "yandex.ru",
     authentication:       "plain",
     enable_starttls_auto: true,
-    user_name:            ENV[SMTP_USERNAME],
-    password:             ENV[SMTP_PASSWORD] }
+    user_name:            ENV['SMTP_USERNAME'],
+    password:             ENV['SMTP_PASSWORD'] }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
