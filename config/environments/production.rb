4a8911d3349147f015ca03cd0430ee0b3d3fa74a
Kadina1988 <kadina.aziev@mail.ru>
2023-05-08 19:42:44 +0300

    gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
     port:                 587,
     domain:               "gmail.com",
     user_name:            'ramazan2015aziev@gmail.com',
-    password:             'shamilek'
+    password:             'shamilek',
     authentication:       "plain",
     enable_starttls_auto: true
     }
