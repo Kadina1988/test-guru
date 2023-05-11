fa04a21bf9040e0f547ee060852a17fa3efd419b
Kadina1988 <kadina.aziev@mail.ru>
2023-05-11 21:42:39 +0300

    dd

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,6 +5,7 @@ set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
+bundle exec rake RAILS_ENV=production db:drop DISABLE_DATABASE_ENVIRONMENT_CHECK=1
 bundle exec rails db:check_protected_environments
 # bundle exec rails db:setup
 # bundle exec rails db:seed
