24ecdfb763d65004aea218d9b8a6bc9200baa2da
Kadina1988 <kadina.aziev@mail.ru>
2023-05-12 12:12:05 +0300

    tls deleted

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'test1-bocz.onrender.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    tls:                  true,
+    # tls:                  true,
     address:              "smtp.yandex.ru",
     port:                 465,
     user_name:            ENV['SMTP_USERNAME'],
