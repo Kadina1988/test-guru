1061f7c1f8d5b2016812078258c6959d84958a91
Kadina1988 <kadina.aziev@mail.ru>
2023-05-12 22:47:31 +0300

    bek

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "new_app_production"
 
   # config.action_mailer.perform_caching = false
-  config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: 'test1-bocz.onrender.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
