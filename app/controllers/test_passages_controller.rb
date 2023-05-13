e56585c1543138d9606dec6a1309ad3fb8705c24
Kadina1988 <kadina.aziev@mail.ru>
2023-05-13 09:37:02 +0300

    later

--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -9,7 +9,7 @@ class TestPassagesController < ApplicationController
     @test_passage.accept!(params[:answer_ids])
 
     if @test_passage.completed?
-      TestMailer.completed_test(@test_passage)
+      TestMailer.completed_test(@test_passage).deliver_later
       redirect_to result_test_passage_path(@test_passage)
     else
       redirect_to test_passage_path
