4a84baf497e36ae6f26feb4c86d5a7329bc0151a
Kadina1988 <kadina.aziev@mail.ru>
2023-05-13 11:27:20 +0300

    yand

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 users = User.create!([
                       { first_name: 'Mike', last_name: 'Tison', email: 'az.ramaz@yandex.ru', password: '1234567shamilek', type: 'Admin'},
-                      { first_name: 'Rama', last_name: 'Mamura', email: 'aziev.ramazan@yandex.ru', password: 'ramazan' }
+                      { first_name: 'Rama', last_name: 'Mamura', email: 'aziev.kadina@yandex.ru', password: 'ramazan' }
                      ])
 categories = Category.create!([{ title: 'Литература' }, { title: 'Информатика' }, { title: 'История' }])
 
