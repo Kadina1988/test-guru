93bc87a920a4efb479702d2568feb6f8347a9a95
Kadina1988 <kadina.aziev@mail.ru>
2023-08-10 16:45:36 +0300

    corrected level names

--- app/helpers/tests_helper.rb
+++ app/helpers/tests_helper.rb
@@ -1,7 +1,7 @@
 module TestsHelper
-  LEVEL = { 1 => 'easy', 2 => 'middle', 3 => 'hard', 4 => 'dificult' }.freeze
+  TEST_LEVELS = { 0 => :easy, 1 => :elementary, 2 => :advanced, 3 => :hard }.freeze
 
-  def test_level(l)
-    l > 4 ? 'superhard' : LEVEL[l]
+  def test_level(test)
+    TEST_LEVELS[test.level] || :hero
   end
 end
